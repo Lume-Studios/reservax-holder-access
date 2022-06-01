9f4a36fe896371005e83e725e93b8b2cc04edda9
Nicolau Riess <nicolauriess@gmail.com>
2022-06-01 11:51:21 -0300

    prod

--- src/index.js
+++ src/index.js
@@ -54,8 +54,9 @@ const isHolder = async () => {
 
 
             if (balanceTotal) {
-                const message = process.env.AUTHENTICATION;
+                const message = "Olá Pistol Holder! Estamos verificando sua wallet para liberar o acesso exclusivo a área do holder";
                 const signature = await ethereum.request({ method: 'personal_sign', params: [message, accounts[0]] });
+                console.log(signature)
                 const response = await axios.post(process.env.SERVER + 'auth/login', {
                     address: accounts[0],
                     projectId: process.env.PROJECT_ID,
